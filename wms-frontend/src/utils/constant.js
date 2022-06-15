ce5b659e7e96144d1864a4cbdfb62e33a69a2127
Edo Novanto <novantoedo@gmail.com>
2022-06-15 09:32:01 +0700

    change endpoint

--- wms-frontend/src/utils/constant.js
+++ wms-frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://127.0.0.1:8000';
+export const BASE_URL = 'https://thawing-brushlands-89668.herokuapp.com';
 
 // READ
 export const GET_ALL_STUFF = BASE_URL + '/api/barangs';
